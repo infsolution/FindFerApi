4f4cf7ef65c2c2b728ce518378e4aae1859d1a32
infsolution <clsinfsolution@gmail.com>
2018-08-27 20:29:12 -0300

    add seeder

--- findfer/database/seeds/DatabaseSeeder.php
+++ findfer/database/seeds/DatabaseSeeder.php
@@ -31,29 +31,45 @@ class DatabaseSeeder extends Seeder
 }
 
 class PostSeeder extends Seeder{
-	public function run()
+    public function run()
     {
-        DB::insert('',[]);
+        DB::insert('insert into posts(url_image, title, content, autor)values(?,?,?)',['https://i.imgur.com/93JIrgs.jpg', 'Conectar feirantes a clientes','O principal objetivo da FindFer é criar um elo entre feirantes e clientes de feiras, por meio de uma ampla divulgação de feiras, produtos de estabelecimentos de feirantes, gerando de forma controlada uma cadeia de interesses voltada para o ecossistema de feiras livres e o comércio de produtos agrícolas e afins.','Cicero']);
+        DB::insert('insert into posts(url_image, title, content, autor)values(?,?,?)',['https://i.imgur.com/UrCRzSN.jpg', 'Mapa com localização das feiras','O app FindFer dispõe de uma sistema de mapas onde o cliente pode encontrar as feiras mais próximas de sua localização , além de todas as feiras cadastradas na plataforma, facilitando a localização de bancas e produtos.','Cicero']);
+        DB::insert('insert into posts(url_image, title, content, autor)values(?,?,?)',['https://i.imgur.com/XkHeQWo.jpg', 'Opções de negociação para clientes','O app FindFer permite que os clientes enviem à plataforma solicitações de PROMOÇÕES, que é uma forma de o cliente negociar com os feirantes, pedindo que os mesmos disponibilizem produtos do seu interesse a um preço específico.','Cicero']);
+        DB::insert('insert into posts(url_image, title, content, autor)values(?,?,?)',['https://i.imgur.com/fx4FDwl.jpg', 'Fidelização e aumento nas vendas','A partir da divulgação de seus produtos os feirantes podem atrair mais clientes para as feiras livres e seus estabelecimentos, pois quando o cliente está bem informado sobre produtos e sua localização exata, surge maior interesse em comprar.','Cicero']);
+        DB::insert('insert into posts(url_image, title, content, autor)values(?,?,?)',['https://i.imgur.com/urZUMYl.jpg', 'Vendas pela plataforma<','A plataforma FindFer permite que os feirantes comercializem seus produtos direto do aplicativo, gerando comodidade para clientes que podem pagar com cartão de crédito ou débito.','Cicero']);
+        DB::insert('insert into posts(url_image, title, content, autor)values(?,?,?)',['https://i.imgur.com/hzcW8mA.jpg', 'O cliente recebe seu produto em casa','O cliente pode escolher em ir buscar o produto na banca ou receber em casa de forma cômoda e segura, em um tempo determinado e a qualidade dos produtos garantida..','Cicero']);
     }
 }
 class SeriveSeeder extends Seeder{
-	public function run()
+    public function run()
     {
-        DB::insert('',[]);
+        DB::insert('insert into services(title, description, value)values(?,?,?)',['Venda de creditos de promoção','O feirante cadastrado em uma conta free, pode comprar creditos para poder criar promoções, que é um beneficio de contas premium.',10.99]);
+        //DB::insert('insert into services(title, description, value)',[]);
+        //DB::insert('insert into services(title, description, value)',[]);
     }
 }
 class AboultSeeder extends Seeder{
-	public function run()
+    public function run()
     {
-        DB::insert('',[]);
+        DB::insert('insert into  aboults(title, description, founder,date_founder, url_image_startup, url_image_founder)vales(?,?,?,?,?,?)',['FindFer - Conectando Feirantes a clientes','<p>A <strong>FindFer</strong> é uma plataforma que visa conectar de forma fácil e inteligente, feirantes e potenciais clientes de feiras com uma ampla divulgação de feiras e produtos e crianco um contato direto dos feirantes com sues clientes.</p>
+
+<p>A FindFer disponibiliza para os feirantes uma comunicação direta com cada um de seus clientes de forma automática, sempre que o feirante anuncía seu produto seus clientes são notiifcados, e sempre que um cliente está na feira os feirante já tem conhecimento e pode criar promoções especificas de acordo com o perfil do cliente.</p>
+
+<p>O clientes também podem pedir promoções! Quando um cliente está na feira que não encotra aquilo que procura com um bom preço ele pode <strong>"Pedir uma Promoção"</strong>  especificando o produto e quanto estão dispostos a pagar e os feirantes habilitados podem ou não fazer essa promoção.</p>','Cicero Leonardo da Silva','2017-04-17',null,null]);
     }
 }
 class ContactSeeder extends Seeder{
-	public function run()
+    public function run()
     {
-        DB::insert('',[]);
+        DB::insert('insert into  contacts(type,value)vales(?,?)',['Telefone','(86)988698580']);
+        DB::insert('insert into  contacts(type,value)vales(?,?)',['Email','contato@findfer.com.br']);
+        DB::insert('insert into  contacts(type,value)vales(?,?)',['Email','clsinfsolution@gmail.com']);
+        DB::insert('insert into  contacts(type,value)vales(?,?)',['Mensagem no site','www.findfer.com.br/contacts']);
     }
 }
+
+
 
 class AddressSeeder extends Seeder{
 	public function run()

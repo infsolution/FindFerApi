2f1d237776d3d646776022f5658b2b17767183fe
infsolution <clsinfsolution@gmail.com>
2018-08-28 16:38:42 -0300

    Create authetication with passport

---
+++ findfer/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace findfer\Http\Controllers\Api;
+
+use Validator;
+use findfer\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use findfer\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public $successStaus = 200;
+
+    public function login(){
+    	if(Auth::attempt(['email'=>request('email'), 'password'=>request('password')])){
+    		$user = Auth::user();
+    		$success['token'] = $user->createToken('findfer')->accessToken;
+    		return response()->json(['success'=>$success],$this->successStaus);
+    	}
+    	return response()->json(['error'=>'Unauthorised'],401);
+    }
+
+    public function register(Request $request){
+    	$validator = Validator::make($request->all(),[
+    		'name'=>'required',
+    		'email'=>'required|email',
+    		'password'=>'required',
+    		'c_password'=>'required|same:password',
+    		]);
+    	if($validator->fails()){
+    		return response()->json(['error'=>$validator->erros()],401);
+    	}
+    	$input = $request->all();$input['password'] = bcrypt($input['password']);
+    	$user = User::create($input);
+    	$success['token'] = $user->createToken('findfer')->accessToken;
+    	$success['name'] = $user->name;
+    	return response()->json(['success'=>$success], $this->successStaus);
+    }
+
+    function details(){
+    	$user = Auth::user();
+    	return response()->json(['success'=>$user],$this->successStaus);
+    }
+}
--- findfer/routes/api.php
+++ findfer/routes/api.php
@@ -13,12 +13,21 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+Route::post('login', 'Api\UserController@login')->name('login');
+Route::post('create', 'Api\UserController@create')->name('create.user');
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'Api\UserController@details');
+
 });
+
 
 Route::resource('/market','MarketController');
 Route::prefix('poster')->group(function(){
-	Route::get('','PosterController@index')->name('posters');
+	//Route::get('','PosterController@index')->name('posters');
+	Route::middleware('auth:api')->get('','PosterController@index')->name('posters');
 	Route::post('/create','PosterController@create')->name('create.poster');
 });
